627bb6f2c1d198f78381ede5127c623d0ce240db
Stephani Moroni Bishop <smoronibishop@netflix.com>
2020-02-12 22:25:12 -0800

    Update package version

--- stories/versions.ts
+++ stories/versions.ts
@@ -1,1 +1,1 @@
-export const versions = {"@catastrophee/styles":"0.7.19","catastropheemedia":"0.0.35","@catastrophee/ui":"0.7.30","catastropheetypeahead":"0.5.14","@catastrophee/models":"0.6.31","catastropheepdf":"0.5.5"}
+export const versions = {}
