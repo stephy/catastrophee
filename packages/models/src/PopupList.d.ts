a93ce18f2381e8212555f220ab781ffe6b71d51e
Stephani Moroni Bishop <smoronibishop@netflix.com>
2020-02-12 23:24:52 -0800

    Remove d.ts

--- packages/models/src/PopupList.d.ts
+++
@@ -1,8 +0,0 @@
-import { Popup } from "./Popup";
-export declare class PopupList {
-    idToEvent: {
-        [id: string]: Popup;
-    };
-    static fromValue(value: any): PopupList;
-    getList(): any[];
-}
